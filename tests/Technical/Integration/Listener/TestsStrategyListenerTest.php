0ce7523d5fe20663b991ec8970ea651617de8a4c
Yoanm <yoanm@users.noreply.github.com>
2017-01-14 01:25:33 +0100

    Improve tests

--- tests/Technical/Integration/Listener/TestsStrategyListenerTest.php
+++ tests/Technical/Integration/Listener/TestsStrategyListenerTest.php
@@ -2,10 +2,12 @@
 namespace Technical\Integration\Yoanm\PhpUnitExtended\Listener;
 
 use Prophecy\Argument;
-use Prophecy\Prophecy\ObjectProphecy;
 use Technical\Integration\Yoanm\PhpUnitExtended\Mock\TestCaseMock;
 use Yoanm\PhpUnitExtended\Listener\TestsStrategyListener;
 
+/**
+ * @covers Yoanm\PhpUnitExtended\Listener\TestsStrategyListener
+ */
 class TestsStrategyListenerTest extends \PHPUnit_Framework_TestCase
 {
     /** @var TestsStrategyListener */
@@ -59,6 +61,87 @@ class TestsStrategyListenerTest extends \PHPUnit_Framework_TestCase
             $expectedCoverageData[self::TEST_FILENAME][0] = [];
         }
         $this->assertSame($expectedCoverageData, $coverage->getData(true));
+    }
+
+    /**
+     * @dataProvider getExceptionMessageProvider
+     *
+     * @param string $exceptionClass
+     * @param string $expectedReason
+     */
+    public function testAddRiskyTest($exceptionClass, $expectedReason, $called = true, $exceptionMessage = null)
+    {
+        $time = 0.3;
+
+        /** @var \PHPUnit_Framework_TestCase|ObjectProphecy $test */
+        $test = $this->prophesize(\PHPUnit_Framework_TestCase::class);
+        /** @var \PHPUnit_Framework_TestResult|ObjectProphecy $testResult */
+        $testResult = $this->prophesize(\PHPUnit_Framework_TestResult::class);
+        /** @var \Exception|ObjectProphecy $exception */
+        $exception = new $exceptionClass($exceptionMessage);
+
+        $test->getTestResultObject()
+            ->willReturn($testResult->reveal())
+            ->shouldBeCalled();
+        $testResult->addFailure(
+            $test,
+            Argument::allOf(
+                Argument::type(\PHPUnit_Framework_AssertionFailedError::class),
+                Argument::that(function (\PHPUnit_Framework_AssertionFailedError $arg) use ($expectedReason) {
+                    return preg_match(sprintf('#%s#', preg_quote($expectedReason)), $arg->getMessage());
+                })
+            ),
+            $time
+        )
+            ->shouldBeCalledTimes($called ? 1 : 0);
+
+        if (\PHPUnit_Framework_OutputError::class === $exceptionClass) {
+            $testResult->getCodeCoverage()
+                ->willReturn(null)
+                ->shouldBeCalled();
+        }
+
+        $this->listener->addRiskyTest($test->reveal(), $exception, $time);
+    }
+
+    /**
+     * @return array
+     */
+    public function getExceptionMessageProvider()
+    {
+        return [
+            'Output exception' => [
+                'exceptionClass' => \PHPUnit_Framework_OutputError::class,
+                'expectedMessage' => 'No output during test',
+            ],
+            'Coverage exception' => [
+                'exceptionClass' => \PHPUnit_Framework_UnintentionallyCoveredCodeError::class,
+                'expectedMessage' => 'Executed code must be defined with @covers and @uses annotations',
+            ],
+            'Globals manipulation - globals' => [
+                'exceptionClass' => \PHPUnit_Framework_RiskyTestError::class,
+                'expectedMessage' => 'Global variable manipulation during test',
+                'called' => true,
+                'exceptionMessage' => '--- Global variables before the test',
+            ],
+            'Globals manipulation - static' => [
+                'exceptionClass' => \PHPUnit_Framework_RiskyTestError::class,
+                'expectedMessage' => 'Static attribute manipulation during test',
+                'called' => true,
+                'exceptionMessage' => '--- Static attributes before the test',
+            ],
+            'Test nothing' => [
+                'exceptionClass' => \PHPUnit_Framework_RiskyTestError::class,
+                'expectedMessage' => 'Test that do not test anything',
+                'called' => true,
+                'exceptionMessage' => 'This test did not perform any assertions',
+            ],
+            'other exceptions' => [
+                'exceptionClass' => \Exception::class,
+                'expectedMessage' => 'Risky test',
+                'called' => false,
+            ],
+        ];
     }
 
     /**
